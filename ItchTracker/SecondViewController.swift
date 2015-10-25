c6c522f594f2042dcb1f4b1a4e576ba3f73db0b8
Fredrik Josefsson <fredrik.josefsson@akestamholst.se>
2015-10-25 21:41:10 +0100

    Initial Commit

---
+++ ItchTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ItchTracker
+//
+//  Created by Fredrik Josefsson on 25/10/15.
+//  Copyright © 2015 Fredrik Josefsson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
